aca55fc25025ff86be7c29fa1d575970a5973ce5
Sanjay Charitesh Makam <158481356+makamsanjay@users.noreply.github.com>
2025-04-08 01:15:48 +0530

    Add files via upload

---
+++ src/util/GetData.js
@@ -0,0 +1,16 @@
+/*
+
+https://solace.ist.rit.edu/~dsbics/proxy/
+const proxyServer = 'https://people.rit.edu/~dsbics/proxy/https://ischool.gccis.rit.edu/api/';
+
+*/
+
+const proxyServer = 'https://people.rit.edu/~dsbics/proxy/https://ischool.gccis.rit.edu/api/';
+
+//endpoint ex: about/
+async function getData(endpoint){
+    const result = await fetch(`${proxyServer}${endpoint}`);
+    return await result.json();
+}
+
+export default getData;
